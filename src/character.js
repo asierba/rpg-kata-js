81799927d0717c8610933c88002b9b88c9a65d0c
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2019-04-28 01:22:08 +0200

    characters can damage each other

--- src/character.js
+++ src/character.js
@@ -1,5 +1,12 @@
 const createCharacter = () => {
-    return { isAlive: true, health: 1000, level: 1 }
+    return {
+        isAlive: true,
+        health: 1000,
+        level: 1,
+        attacks: (target, damage) => {
+            target.health -= damage
+        }
+    }
 }
 
 module.exports = { createCharacter }
--- tests/character.test.js
+++ tests/character.test.js
@@ -1,16 +1,26 @@
 const { createCharacter } = require('../src/character')
 
+describe('a character should', () => {
+    test('be born alive', () => {
+        expect(createCharacter().isAlive).toBe(true)
+    })
 
+    test('be born with 1000 health', () => {
+        expect(createCharacter().health).toBe(1000)
+    })
 
-test('a character is born alive', () => {
-    expect(createCharacter().isAlive).toBe(true)
+    test('be born with level 1', () => {
+        expect(createCharacter().level).toBe(1)
+    })
+
+    test('deal damage to another player', () => {
+        const character1 = createCharacter()
+        const character2 = createCharacter()
+
+        character1.attacks(character2, 50)
+
+        expect(character2.health).toBe(950)
+    })
 })
 
-test('a character is born with 1000 health', () => {
-    expect(createCharacter().health).toBe(1000)
-})
-
-test('a character is born with level 1', () => {
-    expect(createCharacter().level).toBe(1)
-})
 
