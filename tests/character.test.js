cb2e645dc29e36ed2a5b71c4ff8017006ffb385d
Asier Barrenetxea <asierba@gmail.com>
2019-04-28 01:09:24 +0200

    character has a level

--- tests/character.test.js
+++ tests/character.test.js
@@ -1,10 +1,16 @@
 const { createCharacter } = require('../src/character')
 
+
+
 test('a character is born alive', () => {
     expect(createCharacter().isAlive).toBe(true)
 })
 
 test('a character is born with 1000 health', () => {
     expect(createCharacter().health).toBe(1000)
+})
+
+test('a character is born with level 1', () => {
+    expect(createCharacter().level).toBe(1)
 })
 
