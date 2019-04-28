b0ae293a5d67f2430d0314308548f437496b69d3
Asier Barrenetxea <asierba@gmail.com>
2019-04-28 02:08:43 +0200

    characters can heal each other

--- src/character.js
+++ src/character.js
@@ -1,15 +1,21 @@
 const createCharacter = () => {
     let health = 1000
+    const isAlive = () => health > 0
 
     return {
-        isAlive: () => health > 0,
+        isAlive,
         getHealth: () => health,
         setHealth: value => {
-            health = value < 0 ? 0 : value;
+            if (isAlive()) {
+                health = value < 0 ? 0 : value;
+            }
         },
         level: 1,
         damages: (target, damage) => {
             target.setHealth(target.getHealth() - damage)
+        },
+        heals: (target, amountToHeal) => {
+            target.setHealth(target.getHealth() + amountToHeal)
         }
     }
 }
